347d426310f87c6a650bcf2bee95fbdb70286d3c
smarbos <smarbos@the-pep-machine.wtf>
2018-06-25 21:51:00 -0300

    [DM] Here we can attach global hooks for the app.

---
+++ src/app.hooks.js
@@ -0,0 +1,34 @@
+'use strict';
+// Application hooks that run for every service
+
+module.exports = {
+  before: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+
+  after: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  },
+
+  error: {
+    all: [],
+    find: [],
+    get: [],
+    create: [],
+    update: [],
+    patch: [],
+    remove: []
+  }
+};
