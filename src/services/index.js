01fdcce8a6cee8e8445b8958322933a2b6c05834
José Luis Di Biase <josx@interorganic.com.ar>
2018-07-03 11:05:04 -0300

    Avoid console to debug app
    Signed-off-by: José Luis Di Biase <josx@interorganic.com.ar>

--- src/services/index.js
+++ src/services/index.js
@@ -8,9 +8,6 @@ const mongoose = require('mongoose');
 
 module.exports = function() {
   const app = this; // eslint-disable-line no-unused-vars
-  console.log('[APP]');
-  console.log(app);
-  console.log('[APP]');
   // mongoose.Promise = global.Promise;
   // mongoose.connect(app.get('mongodb'), { useMongoClient: true });
 
